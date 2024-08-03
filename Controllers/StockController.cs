135cf405598815612c022a9b736e1f79628eb572
madoomino <sasa6521d@gmail.com>
2024-08-03 07:18:54 +0300

    feat: adding Stock CRUD

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -22,10 +22,8 @@ public class StockController(ApplicationDbContext context) : ControllerBase
   public IActionResult GetStock([FromRoute] int id)
   {
     var stock = _context.Stocks.Find(id);
-    if (stock == null)
-    {
-      return NotFound();
-    }
+    if (stock == null) return NotFound();
+
     return Ok(stock.ToStockDto());
   }
 
@@ -35,7 +33,45 @@ public class StockController(ApplicationDbContext context) : ControllerBase
     var stock = stockDto.ToStockFromCreateDto();
     _context.Add(stock);
     _context.SaveChanges();
-    return CreatedAtAction(nameof(GetStock), new {id = stock.Id}, stock.ToStockDto());
+    return CreatedAtAction(nameof(GetStock), new { id = stock.Id }, stock.ToStockDto());
   }
 
+  [HttpPost("/many")]
+  public IActionResult CreateStocks([FromBody] CreateStockRequestDto[] stockDtos)
+  {
+    var stocks = stockDtos.Select(s => s.ToStockFromCreateDto()).ToList();
+    _context.AddRange(stocks);
+    _context.SaveChanges();
+
+    var stockDtosToReturn = stocks.Select(s => s.ToStockDto()).ToList();
+    return CreatedAtAction(nameof(GetStock), null, stockDtosToReturn);
+  }
+
+  [HttpPut("{id}")]
+  public IActionResult UpdateStock([FromRoute] int id, UpdateStockRequestDto updateStockDto)
+  {
+    var stock = _context.Stocks.FirstOrDefault(s => s.Id == id);
+    if (stock == null) return NotFound();
+
+    stock.CompanyName = updateStockDto.CompanyName;
+    stock.MarketCap = updateStockDto.MarketCap;
+    stock.LastDiv = updateStockDto.LastDiv;
+    stock.Industry = updateStockDto.Industry;
+    stock.Purchase = updateStockDto.Purchase;
+    stock.Symbol = updateStockDto.Symbol;
+
+    _context.SaveChanges();
+    return Ok(stock.ToStockDto());
+  }
+
+  [HttpDelete("{id}")]
+  public IActionResult DeleteStock([FromRoute] int id)
+  {
+    var stock = _context.Stocks.FirstOrDefault(s => s.Id == id);
+    if (stock == null) return NotFound();
+
+    _context.Stocks.Remove(stock);
+    _context.SaveChanges();
+    return NoContent();
+  }
 }
